28707575a345d2fa211c8476b1c244b072f203e6
itstheabhiiigmailcom <154773883+itstheabhiiigmailcom@users.noreply.github.com>
2025-02-06 10:12:38 +0530

    some changes made in controller to get student based on name or id

--- routes/student.js
+++ routes/student.js
@@ -1,29 +1,52 @@
-import { Router } from "express";
-import { addStudent, deleteStudentById, getAllStudents, getStudentById, searchStudent, updateStudentById, uploadFile } from "../controllers/student.controller.js";
-import { validateStudent } from "../middlewares/studentDataValidation.middleware.js";
+import { Router } from 'express';
+import {
+  addStudent,
+  deleteStudentById,
+  getAllStudents,
+  getStudentById,
+  searchStudent,
+  updateStudentById,
+  uploadFile,
+} from '../controllers/student.controller.js';
+import { validateStudent } from '../middlewares/studentDataValidation.middleware.js';
 import { login, refreshTheToken } from '../controllers/auth.controller.js';
 import { authenticate } from '../middlewares/auth.middleware.js';
-import { upload } from "../middlewares/multer.js";
-import { getSuggestions, searchItems, searchStudent2 } from "../controllers/search.controller.js";
+import { upload } from '../middlewares/multer.js';
+import {
+  getSuggestions,
+  searchItems,
+  searchStudent2,
+} from '../controllers/search.controller.js';
 
 const appRouter = Router();
 
-
-appRouter.post("/login", login);
-appRouter.post("/refresh", refreshTheToken);
+appRouter.post('/login', login);
+appRouter.post('/refresh', refreshTheToken);
 
 // secure Routes
 // CRUD operations
-appRouter.post('/student', upload.single("file"), authenticate, validateStudent(false) ,addStudent);     // create and validate before add
-appRouter.get('/student', authenticate ,getAllStudents);      // read all
-appRouter.get('/student/:student_id', authenticate, getStudentById);    // read
-appRouter.put('/student/:student_id', upload.single("file"), authenticate, validateStudent(true), updateStudentById);     // update
-appRouter.delete('/student/:student_id', authenticate, deleteStudentById);  // delete
+appRouter.post(
+  '/student',
+  upload.single('file'),
+  authenticate,
+  validateStudent(false),
+  addStudent
+); // create and validate before add
+appRouter.get('/student', authenticate, getAllStudents); // read all
+appRouter.get('/student/:student_id?/:first_name?', getStudentById); // read
+appRouter.put(
+  '/student/:student_id',
+  //   upload.single('file'),
+  //   authenticate,
+  //   validateStudent(true),
+  updateStudentById
+); // update
+appRouter.delete('/student/:student_id', authenticate, deleteStudentById); // delete
 
-appRouter.post('/search', authenticate, searchStudent)
-appRouter.post('/upload', upload.single("file"), uploadFile);
+appRouter.post('/search', authenticate, searchStudent);
+appRouter.post('/upload', upload.single('file'), uploadFile);
 
-appRouter.post('/search2', authenticate, searchItems)       // query type url searching technique
+appRouter.post('/search2', authenticate, searchItems); // query type url searching technique
 
 // Search Engine
 appRouter.post('/search3', searchStudent2);
